7402468a6670bb6beeff8bd17d493a55c0b5e3eb
Ilza <ilzammed2@gmail.com>
2020-06-08 15:26:07 -0300

    resoluçao exercicios objetos 3 e 4

---
+++ aula-01/objetos/exercicio-03.js
@@ -0,0 +1,11 @@
+/*Crie um objeto curso que receba as propriedades: nome, período, professora, nomeCurso. 
+Em seguida, acesse as propriedades do objeto.*/ 
+
+const curso = {
+    nome: 'Ilza',
+    periodo: '2º período',
+    professora: 'Melinda',
+    nomeCurso: 'Bootcamp Front-End'
+}
+
+console.log(`Nome: ${curso.nome}, Período: ${curso.periodo}, Professora: ${curso.professora} e Nome do curso: ${curso.nomeCurso}`)
---
+++ aula-01/objetos/exercicio-04.js
@@ -0,0 +1,14 @@
+// Com no mínio 5 propriedades 
+// diferentes 
+// minimo* 
+// Crie um objeto que contenha: arrays, listas, booleanos e números. Crie no mínimo 5 propriedades
+
+const minhasCoisas = {
+    nome: 'Ilza',
+    profissaoDev: true,
+    idade: 32,
+    livrosPreferidos: ['Persuasão', 'Saga Harry Potter', 'O homem que matou Getúlio Vargas', 'Outlander'],
+    seriesPreferidas: ['Outlander', 'How I Met Your Mother', 'Friends', 'Game of Thrones']
+}
+
+console.log(minhasCoisas)
